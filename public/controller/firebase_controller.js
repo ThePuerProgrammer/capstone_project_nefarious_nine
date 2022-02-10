5f400ec21a98a2b2bd8001c424451a92b360eb6a
piperbacker <piper.backer@gmail.com>
2022-02-10 12:04:30 -0600

    added firebase functions

--- public/controller/firebase_controller.js
+++ public/controller/firebase_controller.js
@@ -1,1 +1,8 @@
-// Empty for now. TODO
+import * as Constant from '../model/constant.js'
+
+export async function createDeck(deck) {
+    const ref = await firebase.firestore()
+        .collection(Constant.collectionName.OWNED_DECKS)
+        .add(deck.serialize());
+    return ref.id;
+}
