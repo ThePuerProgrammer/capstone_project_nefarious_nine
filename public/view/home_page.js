2ce46749fe72679e527762f087342c08abf7ca74
piperbacker <piper.backer@gmail.com>
2022-02-09 18:02:21 -0600

    add new deck button

--- public/view/home_page.js
+++ public/view/home_page.js
@@ -1,11 +1,12 @@
 import * as Elements from './elements.js'
 
 export function addEventListeners() {
-    Elements.menuHome.addEventListener('click', async () => {
+    Elements.menuHome.addEventListener('click', async() => {
         await home_page();
     });
 }
 
 export async function home_page() {
-    Elements.root.innerHTML = `HOME PAGE`;
+    Elements.root.innerHTML =
+        `<button type="button" class="btn btn-primary btn-lg">Large button</button>`;
 }
