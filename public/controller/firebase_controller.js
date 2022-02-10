27565a7ab8ba261e47dbb0a9816536bef31623da
Cody <ccolburn@uco.edu>
2022-02-10 11:00:10 -0600

    added comments re: timestamps and added deleteDeck

--- public/controller/firebase_controller.js
+++ public/controller/firebase_controller.js
@@ -1,1 +1,15 @@
-// Empty for now. TODO
+import * as Constant from '../model/constant.js'
+
+/* when the function for creating a deck is written
+    uncomment the following line to allow for a timestamp
+    of when the deck was created to be retained in Firestore.
+    This can also be utilized for the entire deck or specific flashcards.
+    Whatever utility we need it for. - Cody
+*/
+    // const data = flashcard.toFirestore(Date.now());
+
+// Function to allow for deletion of deck
+// TODO: write Firebase side rules to set permissions for deck deletion
+export async function deleteDeck(docID){
+    await firebase.firestore().collection(Constant.collectionName.FLASHCARDS).doc(docID).delete();
+}
