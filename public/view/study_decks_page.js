4479852dd0b2bbe062c5e163f00bba604196b705
noah-ws <noahstjohn@ymail.com>
2022-02-10 23:52:36 -0600

    added container for test deck list to elements

--- public/view/study_decks_page.js
+++ public/view/study_decks_page.js
@@ -12,6 +12,7 @@ export function addEventListeners() {
         await study_decks_page();
     });
 
+    // CREATE A DECK Submit button event listener
     Elements.formCreateDeck.addEventListener('submit', async e => {
         e.preventDefault();
         const name = e.target.name.value;
@@ -30,13 +31,55 @@ export function addEventListeners() {
             const docId = await FirebaseController.createDeck(deck);
             deck.docId = docId;
         } catch (e) {
-            if (Constant.DEV) console.log(e);
+            if (Constant.DEV) 
+                console.log(e);
         }
 
-    })
+    });
 
-    // Elements.formCreateAFlashcard.addEventListener('submit', createFlashCard);
+    // CREATE A FLASHCARD Submit butotn event listener
+    // TODO: move actual work into its own function and just pass function name
+    Elements.formCreateAFlashcard.addEventListener('submit', async e => {
+        e.preventDefault();
+        const formData = e.target;
+        
+        // Getting contents of flashcard
+        const question = formData.question.value;
+        // Needs Answer
+        // Needs Incorrect Answers
+        // Needs isMultipleChoice
+        const imageURL = "TESTING";
+        const imageName = "TESTING";
+
+        // TODO: const deckDocIDReceivingNewFlashcard = "......."
+
+        const flashcard = new Flashcard({
+            question,
+            imageURL,
+            imageName
+        });
+
+        try {
+            const docId = await FirebaseController.createFlashcard(deckDocIDReceivingNewFlashcard, flashcard);
+            flashcard.docId = docId;
+            
+            if (Constant.DEV) {
+                console.log(`Flashcard created in deck with doc id [${deckDocIDReceivingNewFlashcard}]:`);
+                console.log("Flashcard Contents: ");
+                console.log(JSON.stringify(flashcard));
+            }
+        } catch (e) {
+            if (Constant.DEV) 
+                console.log(e);
+        }
+
+    });
+    // Elements.formCreateAFlashcard.addEventListener('submit', createFlashcard);
 }
+
+// export async function createFlashcard(formData) {
+//     e.preventDefault();
+// }
 
 
 export async function study_decks_page() {
@@ -47,7 +90,7 @@ export async function study_decks_page() {
     html += '<h1> Study Decks </h1>';
     //Allows for the create a flashcard button
     html += `
-        <button class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#modal-create-a-flashcard"> + Create Flashcard</button>
+        <button id="${Constant.htmlIDs.buttonShowCreateAFlashcardModal}" class="btn btn-outline-danger"> + Create Flashcard</button>
     `;
 
     // Solution for merging Piper's 'create_deck_deck_title branch
@@ -58,4 +101,27 @@ export async function study_decks_page() {
     `;
 
     Elements.root.innerHTML = html;
+
+    const buttonShowCreateAFlashcardModal = document.getElementById(Constant.htmlIDs.buttonShowCreateAFlashcardModal);
+
+    // Manually Opening
+    buttonShowCreateAFlashcardModal.addEventListener('click', async e => {
+        e.preventDefault();
+
+        const listOfTestDecks = await FirebaseController.getAllTestingDecks();
+
+        for (var deck in listOfTestDecks) {
+            document.getElementById('container-for-test-deck-list').innerHTML += `
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                <label class="form-check-label" for="flexRadioDefault2">
+                Default checked radio
+                </label>
+            `;
+        }
+
+        
+        
+        // Constant.openModal(Constant.htmlIDs.modalCreateAFlashcard);
+        $(`#${Constant.htmlIDs.modalCreateAFlashcard}`).modal('show');
+    });
 }
