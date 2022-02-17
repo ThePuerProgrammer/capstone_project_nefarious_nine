7a8b5e385303e4819f50adf00a24f4f20237fa3a
StephenSandini <96503276+StephenSandini@users.noreply.github.com>
2022-02-17 00:51:30 -0600

    edit-modal-loads-info-lacking-listeners

--- public/controller/edit_flashcard.js
+++ public/controller/edit_flashcard.js
@@ -7,6 +7,10 @@ import * as Elements from '../view/elements.js'
 // let imageFile2UploadAnswer;
 // let imageFile2UploadQuestion;
 
+let imageAnswer = Elements.formEditFlashcard.answerImageContainer;
+let imageQuestion = Elements.formEditFlashcard.questionImageContainer;
+
+
 
 export async function edit_flashcard(uid, deckId, docID){
     //console.log(docID);
@@ -21,5 +25,72 @@ export async function edit_flashcard(uid, deckId, docID){
         if(Constant.DEV) console.log(e);
         Utilities.info('Error Firebase Controller', JSON.stringify(e));
         return;
+    }
+
+    //Getting Elements from Firebase to Edit Modal
+    Elements.formEditFlashcard.form.docId.value = flashcard.docId;
+    Elements.formEditFlashcard.form.questionImageName.value = flashcard.questionImageName;
+    Elements.formEditFlashcard.form.answerImageName.value = flashcard.answerImageName;
+    Elements.formEditFlashcard.form.question.value = flashcard.question;
+    Elements.formEditFlashcard.form.answer.value = flashcard.answer;
+    Elements.formEditFlashcard.questionImageTag.src = flashcard.questionImageURL;
+    Elements.formEditFlashcard.answerImageTag.src = flashcard.answerImageURL;
+
+    //Verifying Toggles
+    const isMultipleChoice = Elements.formEditFlashcard.multipleChoiceToggle;
+    const isQuestionImage = Elements.formEditFlashcard.questionImageToggle;
+    const isAnswerImage = Elements.formEditFlashcard.answerImageToggle;
+
+    if(!flashcard.questionImageName||flashcard.questionImageName=='N/A'){
+        isQuestionImage.checked=false;
+        checkImageQuestion();
+    } else {
+        isQuestionImage.checked=true;
+        checkImageQuestion();
+
+    }
+    if(!flashcard.answerImageName||flashcard.answerImageName=='N/A'){
+        isAnswerImage.checked=false;
+        checkImageAnswer();
+    } else {
+        isAnswerImage.checked=true;
+        checkImageAnswer();
+    }
+
+    Elements.modalEditFlashcard.show();
+}
+
+//Method to display the container for question image.
+function checkImageQuestion(){
+    if (Elements.formCheckInputIsImageQuestion.checked) {
+      if(imageQuestion.style.display=='none'){
+        imageQuestion.style.display = 'block';
+      } else{
+        imageQuestion.style.display = 'none';
+      }
+    } else if(!Elements.formCheckInputIsImageQuestion.checked){
+      if(imageQuestion.style.display=='none'){
+        imageQuestion.style.display = 'block';
+      } else{
+        imageQuestion.style.display = 'none';
+      }
+    }
+}
+
+//Method to display the container for answer image.
+function checkImageAnswer(){
+    if (Elements.formCheckInputIsImageAnswer.checked) {
+      if(imageAnswer.style.display=='none'){
+        imageAnswer.style.display = 'block';
+      } else{
+        imageAnswer.style.display = 'none';
+      }
+    }
+    else if(!Elements.formCheckInputIsImageAnswer.checked){
+      if(imageAnswer.style.display=='none'){
+        imageAnswer.style.display = 'block';
+      } else{
+        imageAnswer.style.display = 'none';
+      }
     }
 }
