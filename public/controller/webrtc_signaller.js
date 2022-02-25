d9045efb9076bb43e7cc3eb13e1d3a0e58f4b2f3
Jesse Rankins <jwrankins@gmail.com>
2022-02-25 17:59:49 -0600

    docs moved to own folder. webrtc file init

---
+++ public/controller/webrtc_signaller.js
@@ -0,0 +1,11 @@
+import * as FirebaseController from './firebase_controller.js'
+import * as Constant from '../model/constant.js'
+
+export async function createLobby() {
+
+}
+
+export async function joinLobby(lobbyId) {
+
+}
+
