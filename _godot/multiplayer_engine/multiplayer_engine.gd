b3b4650cc7f82504ddfdd2e0ae6f504356078904
Jesse Rankins <jwrankins@gmail.com>
2022-02-27 01:30:23 -0600

    select a classroom added

--- _godot/multiplayer_engine/multiplayer_engine.gd
+++ _godot/multiplayer_engine/multiplayer_engine.gd
@@ -1,6 +1,7 @@
 extends Node
 
 onready var maxPlayersOptionButton = get_node("Centered/TabContainer/CreateLobby/LobbySettings/MaxPlayersOptionButton")
+onready var selectClassroomsButton = get_node("Centered/TabContainer/CreateLobby/LobbySettings/SelectClassroomOptionsButton")
 
 const collections : Dictionary = {
 	'lobbies' : 'lobbies',
@@ -16,8 +17,27 @@ func _ready():
 	maxPlayersOptionButton.add_item("4")
 	maxPlayersOptionButton.set_item_disabled(0, true)
 	
+	# Parse the current user for their classrooms field
+	var fields = CurrentUser.userDoc.doc_fields
+	var myClassrooms = fields["myClassrooms"]
 	
+	# Get all the classroom docs from the classrooms that the user belongs to
+	for classroom in myClassrooms:
+		var collection : FirestoreCollection = Firebase.Firestore.collection('classrooms')
+		var document_task : FirestoreTask = collection.get(classroom)
+		var document : FirestoreDocument = yield(document_task, "get_document")
+		classrooms[classroom] = document.doc_fields["name"]
 
+	# This label will be disabled
+	selectClassroomsButton.add_item("Select a Classroom")
+	
+	# Add all the users classrooms to the dropdown
+	for classroom in classrooms.values():
+		selectClassroomsButton.add_item(classroom)
+	
+	# Disable label
+	selectClassroomsButton.set_item_disabled(0, true)
+	
 func _on_Back_To_Main_Menu_Button_pressed():
 	if (get_tree().change_scene("res://Menu/MenuScreen.tscn") != OK):
 		print("Failed to change scene")
