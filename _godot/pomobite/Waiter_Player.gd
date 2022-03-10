d1dc8803a053f708c135d866340aaa769f961c97
Jesse Rankins <jwrankins@gmail.com>
2022-03-10 05:50:29 -0600

    host stand, POS, logo

--- _godot/pomobite/Waiter_Player.gd
+++ _godot/pomobite/Waiter_Player.gd
@@ -1,14 +1,16 @@
 extends KinematicBody2D
 
+onready var player2 = get_parent().get_node("Player2")
 export var move_speed = 300.0
 var player = true
 
 func _ready():
 	pass
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	process_movement()
 	process_animation()
+	set_z()
 
 func process_movement():
 	var horizontal 	= Input.get_axis("ui_left", "ui_right")
@@ -17,7 +19,13 @@ func process_movement():
 	velocity.x += horizontal
 	velocity.y += vertical
 	velocity *= move_speed
-	move_and_slide(velocity)
+	var _v = move_and_slide(velocity)
 
 func process_animation():
 	pass
+
+func set_z():
+	if player2.position.y < self.position.y:
+		z_index = 2
+	else:
+		z_index = 1
