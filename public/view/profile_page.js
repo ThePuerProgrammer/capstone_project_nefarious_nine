7338abdb603d6468f68be67d96a896d032c0beba
piperbacker <piper.backer@gmail.com>
2022-03-11 01:07:11 -0600

    adding new user fields

--- public/view/profile_page.js
+++ public/view/profile_page.js
@@ -1,5 +1,7 @@
 import * as Elements from './elements.js'
 import * as Routes from '../controller/routes.js'
+import * as FirebaseController from '../controller/firebase_controller.js'
+import * as Auth from '../controller/firebase_auth.js'
 
 export function addEventListeners() {
     Elements.menuProfile.addEventListener('click', async () => {
@@ -9,5 +11,19 @@ export function addEventListeners() {
 }
 
 export async function profile_page() {
-    Elements.root.innerHTML = `profile page`;
+
+    let user;
+    try {
+        user = await FirebaseController.getUser(Auth.currentUser.uid);
+    }catch (e) {
+        console.log(e);
+    }
+
+    let html = '';
+    html += '<h1> Profile Page </h1>';
+    html += `<p> username: ${user.username}</p>`;
+    html += `<p> pet: ${user.pet}</p>`;
+    html += `<p> pfp: ${user.profilePhotoName}</p>`;
+    
+    Elements.root.innerHTML = html;
 }
