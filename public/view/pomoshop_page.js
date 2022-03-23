8916518aa0093a8536cdf085259eb45c0db3cee4
Cody <ccolburn@uco.edu>
2022-03-23 18:49:09 -0500

    nav bars are dumb

--- public/view/pomoshop_page.js
+++ public/view/pomoshop_page.js
@@ -13,7 +13,31 @@ export async function shop_page() {
     Elements.root.innerHTML = ``;
     let html = '';
 
-    html += 'PomoShop Page';
+    html += `<div id="pomo-sidenav" class="sidenav">
+    <button id="skins-shop-button" type="button" class="btn btn-secondary pomo-bg-color-dark pomo-text-color-light">Skins</button>
+    <button id="accessories-shop-button" type="button" class="btn btn-secondary pomo-bg-color-dark pomo-text-color-light">Accessories</button>
+    </div>
+    <div id="pomoshop"></div>`;
 
-    Elements.root.innerHTML = html;
+    Elements.root.innerHTML += html;
+    const original = html;
+
+    const skinsShopButton = document.getElementById('skins-shop-button');
+    skinsShopButton.addEventListener('click', () =>{
+        Elements.root.innerHTML = original;
+        const skinsShop = '<h3>Skins</h3>';
+        const skinShopTag = document.createElement('div');
+        skinShopTag.innerHTML = skinsShop;
+        document.getElementById('pomoshop').appendChild(skinShopTag);
+    })
+
+    const accessoriesShopButton = document.getElementById('accessories-shop-button');
+    accessoriesShopButton.addEventListener('click', () =>{
+        Elements.root.innerHTML = original;
+        const accessoriesShop = '<h3>Accessories</h3>';
+        const accessoriesShopTag = document.createElement('div');
+        accessoriesShopTag.innerHTML = accessoriesShop;
+        document.getElementById('pomoshop').appendChild(accessoriesShopTag);
+    })
+
 }
