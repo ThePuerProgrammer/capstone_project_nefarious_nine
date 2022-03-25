ccfcfa8e0201d7b604c02773026e4685a6f79867
AppGameBoy <elissglasper@gmail.com>
2022-03-25 15:23:00 -0500

    updates to the player mob and main scene also added a powerup

--- _godot/PomoBlast/PomoBlast.gd
+++ _godot/PomoBlast/PomoBlast.gd
@@ -1,8 +1,9 @@
 extends Node
 
 export (PackedScene) var Mob
+export (PackedScene) var PowerUp = preload("res://PomoBlast/PowerUp.tscn")
 
-
+var score = 0 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Player.start($StartPosition.position)
@@ -18,11 +19,11 @@ func _ready():
 
 func _on_MobTimer_timeout():
 	var mob = Mob.instance()
-	
 	# Choose a random location on Path2D.
 	$MobPath/MobSpawnLocation.offset = randi()
 	# Create a Mob instance and add it to the scene.
-	
+	mob.connect("enemy_died",self,"_on_enemy_died")
+	mob.connect("enemy_ded", $HUD/ScoreLabel,"_on_enemy_ded")
 	add_child(mob)
 	# Set the mob's direction perpendicular to the path direction.
 	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
@@ -44,3 +45,21 @@ func _on_GameTimer_timeout():
 	$MobTimer.stop()
 	print('**********************************Game Timer Has Stopped')
 	pass # Replace with function body.
+	
+	
+func _on_enemy_died(pos):
+	score += 1
+	var p = PowerUp.instance()
+	add_child(p)
+	p.position = pos
+	print("we dead")
+
+
+func _on_Pause_pressed():
+	get_tree().paused = true
+	$HUD/Popup.show()
+
+
+func _on_Resume_pressed():
+	$HUD/Popup.hide()
+	get_tree().paused = false 
