67f1fa40ad1103cefdfb7e4d06cd35c33993f889
darkprincessSM <50896133+darkprincessSM@users.noreply.github.com>
2022-03-27 03:28:58 -0500

    added scene decorations, worked on jump physics, one-way collision platforms, scaled screen

--- _godot/SlowFruit/Boogie_Player.gd
+++ _godot/SlowFruit/Boogie_Player.gd
@@ -2,16 +2,14 @@ extends RigidBody2D
 
 # Character Demo, written by Juan Linietsky.
 
-const WALK_ACCEL = 500.0
-const WALK_DEACCEL = 500.0
+const WALK_ACCEL = 650.0
+const WALK_DEACCEL = 650.0
 const WALK_MAX_VELOCITY = 140.0
-const AIR_ACCEL = 100.0
-const AIR_DEACCEL = 100.0
-const JUMP_VELOCITY = 380
-const STOP_JUMP_FORCE = 450.0
+const AIR_ACCEL = 900.0
+const AIR_DEACCEL = 1100.0
+const JUMP_VELOCITY = 680
+const STOP_JUMP_FORCE = 650.0
 const MAX_FLOOR_AIRBORNE_TIME = 0.15
-
-
 
 
 var anim = ""
@@ -20,6 +18,7 @@ var jumping = false
 var walking = false
 var idle = true
 var stopping_jump = false
+var found_floor = true
 
 var right = true
 var left = false
@@ -35,6 +34,7 @@ var airborne_time = 1e20
 onready var sound_jump = $SoundJump
 onready var sprite = $Sprite
 onready var animation_player = $AnimatedSprite
+onready var ground_ray = get_node("ground_ray")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -45,6 +45,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	change_animation()
+	#is_hitting_obstacle()
+	#is_grounded = ground_ray.is_colliding()
+	#drop_like_a_rock()
+	
 	
 func change_animation():
 	if jumping:
@@ -56,6 +60,8 @@ func change_animation():
 	else:
 		$AnimatedSprite.play("Idle")
 	
+
+
 func setJumping():
 	jumping = true
 	idle = false
@@ -80,16 +86,38 @@ func if_left_in_air():
 	if left:
 		if linear_velocity.x < WALK_MAX_VELOCITY:
 			linear_velocity.x += AIR_ACCEL
+			
+func is_found_floor():
+	if linear_velocity.y < 10 && jump == false:
+		found_floor = true
+		setWalking()		
+	else:
+		found_floor = false
+		setJumping()
+		 
+		
+func is_hitting_obstacle():
+	if !found_floor:
+		if linear_velocity.y == 0 && linear_velocity.x == 0:
+			linear_velocity.y += STOP_JUMP_FORCE
+			
+func drop_like_a_rock():
+	if !found_floor && !jumping:
+		linear_velocity.y += STOP_JUMP_FORCE
 	
 func pull_for_input():
 	if Input.is_action_just_pressed("move_left"):
+		
 		if !jumping:
 			setWalking()
+		if found_floor:
+			print("founbd floor")
 		$AnimatedSprite.flip_h = true			
 		move_left = true
 		left = true
 		right = false
 	if Input.is_action_just_released("move_left"):
+		
 		if !jumping:
 			setIdle()
 		move_left = false
@@ -97,6 +125,10 @@ func pull_for_input():
 		linear_velocity.x -= WALK_DEACCEL
 		
 	if Input.is_action_just_pressed("move_right"):
+		print(linear_velocity.y, "y vel")
+		print(linear_velocity.x, "x vel")
+		if found_floor:
+			print("founbd floor")
 		if !jumping:
 			setWalking()
 		$AnimatedSprite.flip_h = false
@@ -111,20 +143,17 @@ func pull_for_input():
 		linear_velocity.x -= WALK_DEACCEL
 	
 	if Input.is_action_just_pressed("jump"):
-		if linear_velocity.y <= 0:			
+		if linear_velocity.y <= 0:	
+			#if !found_floor:
 			setJumping()
 			stopping_jump = false
-			if_left_in_air()
-			if_right_in_air()
 			applied_force = Vector2(applied_force.y, 0)
 			linear_velocity.y = -JUMP_VELOCITY
 		
 	if Input.is_action_just_released("jump"):
 		stopping_jump = true
-		print(linear_velocity.y)
-		print(linear_velocity.x)
-		#if is on floor set walking
-		setWalking()
+		if found_floor:
+			setWalking()
 		applied_force = Vector2(applied_force.y, 0)
 		linear_velocity.y += STOP_JUMP_FORCE
 	
@@ -147,9 +176,10 @@ func set_movement():
 		
 	if linear_velocity.y > JUMP_VELOCITY:
 		stopping_jump = true
-		linear_velocity.y -= JUMP_VELOCITY
+		linear_velocity.y = JUMP_VELOCITY
 	if linear_velocity.y < -JUMP_VELOCITY:
-		linear_velocity.y = -JUMP_VELOCITY
+		linear_velocity.y = -JUMP_VELOCITY		
+	
 		
 func _integrate_forces(s):
 		
