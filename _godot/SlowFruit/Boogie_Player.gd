965cc8e828fc7a1da2dce9dd993494f60d76256a
darkprincessSM <50896133+darkprincessSM@users.noreply.github.com>
2022-03-27 14:57:37 -0500

    paused main menu music for fruit level

--- _godot/SlowFruit/Boogie_Player.gd
+++ _godot/SlowFruit/Boogie_Player.gd
@@ -39,7 +39,7 @@ onready var animation_player = $AnimatedSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	MenuMusic.get_child(0).stop()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
