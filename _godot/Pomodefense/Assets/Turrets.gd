bc7821930646cae8c1b04697ebee8a038500f725
Cody <ccolburn@uco.edu>
2022-04-03 15:58:14 -0500

    added pomodefense

---
+++ _godot/Pomodefense/Assets/Turrets.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+var enemy_array = []
+var built = false
+var enemy
+var type
+var ready = true
+
+func _ready():
+	if built:
+		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.tower_data[type]["range"]
+
+func _physics_process(delta):
+	if enemy_array.size() != 0 and built:
+		select_enemy()
+		if not get_node("AnimationPlayer").is_playing():
+			turn()
+		if ready:
+			fire()
+	else:
+		enemy = null
+	
+func turn():
+	get_node("cannon").look_at(enemy.position)
+	
+func select_enemy():
+	var enemy_progress_array = []
+	for i in enemy_array:
+		enemy_progress_array.append(i.offset)
+	var max_offset = enemy_progress_array.max()
+	var enemy_index = enemy_progress_array.find(max_offset)
+	enemy = enemy_array[enemy_index]
+
+func fire():
+	ready = false
+	get_node("AnimationPlayer").play("Fire")
+	enemy.on_hit(GameData.tower_data[type]["damage"])
+	yield(get_tree().create_timer(GameData.tower_data[type]["rate"]), "timeout")
+	ready = true
+
+func _on_Range_body_entered(body):
+	enemy_array.append(body.get_parent())
+
+func _on_Range_body_exited(body):
+	enemy_array.erase(body.get_parent())
