9c772e078a6ea27aea09f5f481245b4f6cb4092f
Cody <ccolburn@uco.edu>
2022-04-04 13:58:56 -0500

    made it work better than it did since I'm dumb

--- _godot/Pomodefense/Scenes/UI.gd
+++ _godot/Pomodefense/Scenes/UI.gd
@@ -52,8 +52,6 @@ func _on_PausePlay_pressed():
 		get_parent().cancel_build_mode()
 	if get_tree().is_paused():
 		get_tree().paused = false
-	elif get_parent().enemies_in_wave == 0:
-		get_parent().start_next_wave()
 	else:
 		get_tree().paused = true
 
